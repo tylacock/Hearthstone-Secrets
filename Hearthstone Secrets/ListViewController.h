6886c086fa8458d76a6390d3a71a82910e10ac81
tylacock <tylacock@gmail.com>
2014-01-24 14:12:54 -0500

    First Commit

---
+++ Hearthstone Secrets/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  Hearthstone Secrets
+//
+//  Created by Lacock, Ty on 1/24/14.
+//  Copyright (c) 2014 TWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UITableViewController
+
+@end
