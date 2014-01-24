cbeb95494a99249b59d1eb6eb75f1598bc30c5eb
tylacock <tylacock@gmail.com>
2014-01-24 11:05:23 -0500

    Initial Commit

---
+++ Hearthstone Secrets/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hearthstone Secrets
+//
+//  Created by Lacock, Ty on 1/24/14.
+//  Copyright (c) 2014 TWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
